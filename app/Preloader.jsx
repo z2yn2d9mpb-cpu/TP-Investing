8bd4bc3c31aac303b1f8c255b762851021332bb6
z2yn2d9mpb-cpu <z2yn2d9mpb@privaterelay.appleid.com>
2026-04-05 11:47:43 +0200

    Change logo source from zwart to wit

--- app/Preloader.jsx
+++ app/Preloader.jsx
@@ -43,7 +43,7 @@ export default function Preloader() {
       }}
     >
       <img
-        src="/logo-zwart.svg"
+        src="/logo-wit.svg"
         alt="TrustedPips"
         style={{
           width: 100,
